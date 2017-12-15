0660337c323b91034be56be3c96ad7d169cc7c82
Daiderd Jordan <daiderd@gmail.com>
2017-12-15 23:25:02 +0100

    ruby_1_8_7: add bundler

--- ruby-overlay.nix
+++ ruby-overlay.nix
@@ -5,11 +5,17 @@ let
 in
 
 {
-  ruby-overlay = {
+  ruby-overlay = rec {
     interpreters.ruby_1_8_7 = rec {
       ruby = callPackage ./ruby-1.8.7/ruby.nix {};
       rubygems = callPackage ./ruby-1.8.7/rubygems.nix { inherit ruby; };
       buildRubyGem = callPackage ./ruby-1.8.7/build-gem.nix { inherit ruby rubygems; };
+    };
+
+    packages.ruby_1_8_7 = rec {
+      bundler = super.bundler.override {
+        inherit (interpreters.ruby_1_8_7) ruby buildRubyGem;
+      };
     };
   };
 
